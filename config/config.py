712412e41b9a36ec9225171db56209b7c90cad56
Danilka06 <main@0x1f44.ru>
2024-12-20 16:29:10 +0300

    initial

---
+++ config/config.py
@@ -0,0 +1,35 @@
+import logging
+from dataclasses import dataclass
+
+from dotenv import load_dotenv
+
+from .base import getenv
+
+
+# logging
+def init_logging():
+    logger = logging.getLogger(__name__)
+    logger.setLevel(logging.INFO)
+    handler = logging.FileHandler("app.log")
+
+
+@dataclass
+class Bot:
+    token: str
+
+
+@dataclass
+class Config:
+    bot: Bot
+
+
+def load_config() -> Config:
+    load_dotenv()
+    return Config(
+        bot=Bot(
+            token=getenv("AIOGRAM_TOKEN"),
+        ),
+    )
+
+
+config: Config = load_config()
